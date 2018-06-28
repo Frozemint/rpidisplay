2ef684a1ca783cf74183b0cc742eb66507122ca6
Frozemint <nami5504@gmail.com>
2018-06-27 21:13:06 -0700

    Create sample config file

---
+++ src/config_example.js
@@ -0,0 +1,13 @@
+var config = {
+        WEATHER_API_KEY: '',
+        TRANSLINK_API_KEY: '',
+        PRIMARY_BUS_STOP_ID: '',
+        PRIMARY_BUS_ROUTE: '',
+        SECONDARY_BUS_STOP_ID: '',
+        SECONDARY_BUS_ROUTE: '',
+        PRIMARY_WEATHER_LAT: '',
+        PRIMARY_WEATHER_LON: '',
+        SECONDARY_WEATHER_LAT: '',
+        SECONDARY_WEATHER_LON: '',
+        SECONDARY_WEATHER_STRING: ''
+}
